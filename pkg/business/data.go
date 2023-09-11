19114bb10f945bac46d2550d2f3b52ab5e84bcfc
Hans Ferchland <hans.ferchland@void-ptr.org>
2023-09-11 19:56:40 +0200

    Revert bqck

--- pkg/business/data.go
+++ pkg/business/data.go
@@ -44,23 +44,15 @@ func (d *Data) newSensorValuePoint(
 		"unit":     sensorValue.Unit,
 		"unitName": sensorValue.UnitName,
 	}
-	// Influxdb fields
-	fields := map[string]interface{}{
-		"value":    sensorValue.Value,
-		"deviceId": deviceId,
-		"source":   source,
-		"type":     _business.SensorValueType,
-		"name":     name,
-		"unit":     sensorValue.Unit,
-		"unitName": sensorValue.UnitName,
-	}
 	if sensorName != nil {
 		tags["sensorName"] = *sensorName
-		fields["sensorName"] = *sensorName
 	}
 	if sensorType != nil {
 		tags["sensorType"] = fmt.Sprintf("%d", sensorType)
-		fields["sensorType"] = fmt.Sprintf("%d", sensorType)
+	}
+	// Influxdb fields
+	fields := map[string]interface{}{
+		"value": sensorValue.Value,
 	}
 	util.Log.Debugf("NewPoint@%s %s/%s - %s - %s", t.Local().Format("23:05:00.000"), deviceId+"/"+source, tags, fields)
 	return influxdb2.NewPoint(deviceId+"/"+source, tags, fields, t)
